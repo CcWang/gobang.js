4649f9128d27fe4ee82f08c0d2416d0cc23944a1
cc <chongchong1110@gmail.com>
2017-08-02 00:00:00 -0700

    gobang

--- gobang.js
+++ gobang.js
@@ -30,3 +30,4 @@ We need some changes for the commit
 We need some changes for the commit
 We need some changes for the commit
 We need some changes for the commit
+We need some changes for the commit
